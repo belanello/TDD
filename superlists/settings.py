25a560f9b0ca8a15dd7214e2dd0d791a29f23a28
Ayako Nagao <belanello@live.com>
2021-09-12 11:41:47 +0900

    gitignore.env file

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '(+aun2e8^p-ou5@do=1+gr51ol30*46k_wl1l#0-@%s8odgqq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#ALLOWED_HOSTS = ['*']
+
 
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
